c32839f796fab3226aca1d6c9f2097b3b3a26962
manthinh <kbkl0224@i.softbank.jp>
2022-02-13 12:09:08 +0900

    calculator作成

--- calculator.php
+++ calculator.php
@@ -1,1 +1,23 @@
-これはテストです
+<?php
+
+// クラス定義（実態がない）
+class function_code {
+    // オブジェクト
+    public $a = "テスト\n";
+
+    public function test()
+    {
+        $a = "テスト関数\n";
+        echo $this->a;
+        echo $a;
+    }
+}
+
+// 実体化される（インスタンス化インスタンス化される）
+// $instance = new function_code;
+
+// return $instance->test();
+
+
+
+
