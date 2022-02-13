ef539a9c0ae01c1e713e32ba4e893dfbfb51cacd
manthinh <kbkl0224@i.softbank.jp>
2022-02-13 12:05:00 +0900

    index作成

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include dirname(__FILE__) . '/calculator.php';
+
+
+$instance = new function_code;
+
+echo $instance->test();
+
+
+// $plus = new calculator($inputNum, $inputNum2);
+$plus = new function_code();
+
+echo $plus->test();
