7b40a23036b6d44b7d992b1ff6b9c2e991021f83
manthinh <kbkl0224@i.softbank.jp>
2022-02-15 19:26:12 +0900

    数字入力機能追加

--- calculator.php
+++ calculator.php
@@ -1,4 +1,9 @@
 <?php
+$inputNumber1 = $argv[1];
+$inputNumber2 = $argv[2];
+
+echo "左辺: {$inputNumber1} \n";
+echo "右辺: {$inputNumber2} \n";
 
 // クラス定義（実態がない）
 class function_code {
@@ -12,12 +17,12 @@ class function_code {
         echo $a;
     }
 }
+class function_code {
+    public 
+
+
 
 // 実体化される（インスタンス化インスタンス化される）
 // $instance = new function_code;
 
 // return $instance->test();
-
-
-
-
