b253f323315d0a65cbe69ca6077510a223f94ea2
manthinh <kbkl0224@i.softbank.jp>
2022-02-19 13:34:38 +0900

    条件分岐

--- calculator.php
+++ calculator.php
@@ -3,11 +3,15 @@ $inputNumber1 = $argv[1];
 $inputNumber2 = $argv[2];
 $inputSymbol = $argv[3];
 
-$symbol = substr('+-*/',0,1);
-$symbol2 = substr('+-*/',1,1);
-$symbol3 = substr('+-*/',2,1);
-$symbol4 = substr('+-*/',3,1);
-// $symbol5 = substr('+-*/',4,1);
+// $symbol = substr('+-*/',0,1);
+// $symbol2 = substr('+-*/',1,1);
+// $symbol3 = substr('+-*/',2,1);
+// $symbol4 = substr('+-*/',3,1);
+
+$symbol = "+";
+$symbol2 = "-";
+$symbol3 = "*";
+$symbol4 = "/";
 
 $addition = $inputNumber1 + $inputNumber2;
 $subtraction = $inputNumber1 - $inputNumber2;
@@ -16,17 +20,31 @@ $division = $inputNumber1 / $inputNumber2;
 echo "第一引数: {$inputNumber1} \n";
 echo "第二引数: {$inputNumber2} \n";
 echo "第三引数: {$inputSymbol} \n";
-if ($inputSymbol !== $symbol or $symbol2 or $symbol3 or $symbol4) {
+if ($inputSymbol === $symbol or $inputSymbol === $symbol2 or $inputSymbol === $symbol3 or $inputSymbol === $symbol4) {
     echo "$inputSymbol\n";
 } else {
     echo "文字と0以外の数字を入れてください\n";
 } 
 
-echo "answer: {$addition} \n";
-echo "answer: {$subtraction} \n";
-echo "answer: {$multiplication} \n";
-echo "answer:{$division} \n";
+// echo "answer: {$addition} \n";
+// echo "answer: {$subtraction} \n";
+// echo "answer: {$multiplication} \n";
+// echo "answer:{$division} \n";
 
+if ($inputSymbol === $symbol) {
+    echo "足し算: {$addition} \n";
+} else if ($inputSymbol === $symbol2){
+    echo "引き算: {$subtraction} \n";
+} else if ($inputSymbol === $symbol3){
+    echo "掛け算: {$multiplication} \n";
+} else if ($inputSymbol === $symbol4){
+    echo "割り算:{$division} \n";
+}
+
+
+if($inputSymbol === [1-9]){
+    echo "足し算: {$addition} \n";
+}
 // クラス定義（実態がない）
 // class function_code {
 //     // オブジェクト
