6164ea357290a983c135d57655c832f220320da9
manthinh <kbkl0224@i.softbank.jp>
2022-04-24 01:39:40 +0900

    四月二十四日

---
+++ calculate/2022.4.24/写経.php
@@ -0,0 +1,25 @@
+<?php
+
+use calculate as GlobalCalculate;
+
+$InputNumber1 = $argv[1];
+$InputNumber2 = $argv[2];
+$InputSymbol = $argv[3];
+
+class calculate {
+public function __construct($InputNumber1,$InputNumber2,$InputSymbol)
+{
+    $this->InputNumber1;
+    $this->InputNumber2;
+    $this->InputSymbol;
+}
+
+public function addition()
+{
+    $addition = $this->InputNumber1 + $this->InputNumber2;
+    echo $addition."\n";
+}
+}
+
+$instance = new calculate($InputNumber1,$InputNumber2,$InputSymbol);
+echo $instance->$addition;
