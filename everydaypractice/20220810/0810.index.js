b37dffc81717afb5cb4715202fc99698ffba6caa
manthinh <kbkl0224@i.softbank.jp>
2022-08-10 16:05:24 +0900

    let文は変数の宣言（constとの違いも学ぶ）

--- everydaypractice/20220810/0810.index.js
+++ everydaypractice/20220810/0810.index.js
@@ -6,3 +6,15 @@ h1.textContent = "主題変更";
 const p = document.createElement("p");
 const text = document.createTextNode("文3");
 document.body.appendChild(p).appendChild(text);
+
+function buttonClick(){
+    msg.innerText ='お名前は'+ nameText.value + 'さんですね';
+}
+
+let nameText = document.getElementById('nameText');
+nameText.value = '何か入力して';
+let msg = document.getElementById('msg');
+
+let checkButton = document.getElementById('checkButton');
+checkButton.addEventListener('click',buttonClick);
+
